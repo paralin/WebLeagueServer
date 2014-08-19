ba5ca154fdbd67c1ed72510a6225187b53547d6a
Christian Stewart <kidovate@gmail.com>
2014-08-19 11:33:10 -0700

    Base project

---
+++ WLNetwork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using log4net;
+using Castle.Windsor;
+
+namespace WLNetwork
+{
+	class Program 
+	{
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+			(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		public static void Main (string[] args)
+		{
+			log4net.Config.XmlConfigurator.Configure ();
+			log.Info ("Web League master starting up!");
+			log.Debug ("Setting up Castle Windsor...");
+			var container = new WindsorContainer ();
+			//Install
+			//Set up root component
+			log.Info ("Disposing app...");
+			container.Dispose ();
+		}
+	}
+}
