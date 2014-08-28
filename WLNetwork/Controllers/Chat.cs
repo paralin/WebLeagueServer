8fd07e0fc08952216d04797cc6490ab22715e688
Christian Stewart <kidovate@gmail.com>
2014-08-27 18:23:04 -0700

    Small update

--- WLNetwork/Controllers/Chat.cs
+++ WLNetwork/Controllers/Chat.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Security.Principal;
 using MongoDB.Driver.Builders;
 using Newtonsoft.Json.Linq;
+using WLNetwork.Chat;
 using WLNetwork.Database;
 using WLNetwork.Model;
 using WLNetwork.Properties;
 using XSockets.Core.Common.Socket.Attributes;
+using XSockets.Core.Common.Socket.Event.Arguments;
 using XSockets.Core.XSocket;
 using XSockets.Plugin.Framework.Attributes;
 
@@ -19,6 +22,13 @@ namespace WLNetwork.Controllers
     {
         private static readonly log4net.ILog log =
    log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private ObservableCollection<ChatChannel> ChatChannel = new ObservableCollection<ChatChannel>();
+
+        public Chat()
+        {
+            
+        }
 
         public User User
         {
