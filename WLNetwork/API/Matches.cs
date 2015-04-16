b6eeae15b77244cd93dacf34d7771cc405b54d13
Christian Stewart <kidovate@gmail.com>
2015-04-16 10:43:12 -0700

    Revert "Revert "Add matches api""
    This reverts commit 16797ea1cc1597438edbdb5bdd721ea938bdf7b6.

---
+++ WLNetwork/API/Matches.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Nancy;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WLNetwork.Matches;
+using WLNetwork.Matches.Enums;
+
+namespace WLNetwork.API
+{
+    public class Matches : NancyModule
+    {
+        public Matches()
+        {
+            Get["/api/matches"] = para => ListMatches();
+        }
+
+        private string ListMatches()
+        {
+            JArray arr = new JArray();
+            foreach (var match in MatchesController.Games.Where(m => m.Info.Status == MatchStatus.Play))
+            {
+                JArray plyrs = new JArray();
+                foreach (var plyr in match.Players.Where(m => m.Team == MatchTeam.Dire || m.Team == MatchTeam.Radiant))
+                {
+                    plyrs.Add(new
+                    {
+                        Id = plyr.SID,
+                        Name = plyr.Name,
+                        Rating = plyr.Rating,
+                        Hero = plyr.Hero,
+                        Team = plyr.Team
+                    });
+                }
+                arr.Add(new
+                {
+                    Id = match.Id,
+                    Players = plyrs,
+                    State = match.Setup.Details.State,
+                    StartTime = match.Setup.Details.GameStartTime,
+                    SpectatorCount = match.Setup.Details.SpectatorCount
+                });
+            }
+            return arr.ToString(Formatting.Indented);
+        }
+    }
+}
