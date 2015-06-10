1c232c133bbe947858a0fa795ba895a87d6b70d4
Christian Stewart <kidovate@gmail.com>
2015-06-10 13:04:37 +0100

    Fix start script

--- start.bash
+++ start.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do
   until mono ./WLNetwork.exe; do
-    echo "Server crashed with exit code $?.  RESPAWNING.." >&2
-    sleep 1
+    echo "Server crashed with exit code $?.  RESPAWNING.."
+    sleep 3
   done
 done
