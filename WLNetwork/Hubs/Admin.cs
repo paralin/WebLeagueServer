f30ea238b64cd0b7fc43081801a826c1ffe0694d
Christian Stewart <kidovate@gmail.com>
2015-09-08 23:48:19 -0400

    Added admin method to submit unknown match result

--- WLNetwork/Hubs/Admin.cs
+++ WLNetwork/Hubs/Admin.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
 using Dota2.GC.Dota.Internal;
 using log4net;
 using MongoDB.Driver.Builders;
+using Newtonsoft.Json.Linq;
+using WLNetwork.Chat;
 using WLNetwork.Clients;
 using WLNetwork.Database;
+using WLNetwork.Leagues;
 using WLNetwork.Matches;
 using WLNetwork.Matches.Enums;
+using WLNetwork.Model;
+using WLNetwork.Properties;
+using WLNetwork.Rating;
+using WLNetwork.Utils;
 using MatchType = WLNetwork.Matches.Enums.MatchType;
 
 namespace WLNetwork.Hubs
@@ -112,11 +121,138 @@ namespace WLNetwork.Hubs
                 return "Unable to change 1v1 results.";
             }
 
+            var oldResult = res.Result;
             log.Debug("Request to change result of " + res.Id + " from " + res.Result.ToString("G") + " to " +
                       result.ToString("G"));
             if (!res.AdjustResult(result))
                 return "Don't know how to convert " + res.Result.ToString("G") + " to " + result.ToString("G") + ".";
             res.Save();
+
+            ChatChannel.SystemMessage(res.League, "Admin "+client.User.profile.name+" changed result of "+id+" from "+oldResult.ToString("G")+" to "+res.Result.ToString("G")+".");
+            return null;
+        }
+
+        /// <summary>
+        /// Submit a new match result that isn't in the system. Must be a public match (dotabuff viewable).
+        /// </summary>
+        /// <param name="id">match ID</param>
+        /// <param name="leagueid">league ID</param>
+        /// <returns>Error string or null.</returns>
+        public async Task<string> SubmitMatch(ulong id, string leagueid)
+        {
+            // First check for any existing 
+            var client = Client;
+            if (client?.User == null || !client.User.authItems.Contains("admin")) return "You are not an admin.";
+            log.Debug($"Admin {client.User.profile.name} attempted to submit match {id} to league {leagueid}.");
+            if (string.IsNullOrWhiteSpace(leagueid)) return "Not a valid league ID.";
+            if (id == 0) return "Not a valid match ID.";
+
+            League league;
+            if (!LeagueDB.Leagues.TryGetValue(leagueid, out league)) return $"Unable to find league {leagueid}!";
+
+            MatchResult existing = Mongo.Results.FindOneAs<MatchResult>(Query<MatchResult>.EQ(m => m.Id, id));
+            if (existing != null)
+            {
+                if (existing.Result == EMatchResult.DireVictory || existing.Result == EMatchResult.RadVictory)
+                    return "That match has already been resulted.";
+                else
+                {
+                    log.Debug($"Deleting unknown result {id} in favor of API check for result as per request by admin.");
+                    Mongo.Results.Remove(Query<MatchResult>.EQ(m => m.Id, id));
+                }
+            }
+
+            log.Debug($"Looking up match {id}...");
+            bool radiant_win;
+            DateTime completed = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            uint ticket_id = 0;
+            JToken[] players;
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var res =
+                        await
+                            httpClient.GetStringAsync(
+                                $"https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v001/?key={Settings.Default.SteamAPI}&match_id={id}");
+                    var pars = JObject.Parse(res);
+                    var result = pars["result"];
+                    if (result["error"] != null)
+                    {
+                        return result["error"].Value<string>();
+                    }
+                    radiant_win = result["radiant_win"].Value<bool>();
+                    completed =
+                        completed.AddSeconds(result["start_time"].Value<double>() +
+                                             result["duration"].Value<int>());
+                    if (result["leagueid"] != null)
+                    {
+                        ticket_id = result["leagueid"].Value<uint>();
+                    }
+                    players = result["players"].ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error while trying to check API for "+id, ex);
+                return "Unable to check API, errors occured.";
+            }
+
+            var matchResult = new MatchResult
+            {
+                Id = id,
+                League = league.Id,
+                MatchId = existing?.MatchId,
+                Result = radiant_win ? EMatchResult.RadVictory : EMatchResult.DireVictory,
+                MatchCounted = true,
+                MatchCompleted = completed,
+                MatchType = existing?.MatchType ?? MatchType.StartGame,
+                LeagueSeason = existing?.LeagueSeason ?? league.CurrentSeason,
+                LeagueSecondarySeasons = existing?.LeagueSecondarySeasons ?? league.SecondaryCurrentSeason.ToArray(),
+                TicketId = ticket_id
+            };
+
+            List<MatchResultPlayer> resultPlayers = new List<MatchResultPlayer>(players.Length);
+            // Try to find match players
+            foreach (var plyr in players)
+            {
+                var accid = plyr["account_id"].Value<uint>();
+                var hero = HeroCache.Heros[plyr["hero_id"].Value<uint>()];
+                ulong steamid = accid.ToSteamID64();
+                var user = Mongo.Users.FindOneAs<User>(Query<User>.EQ(m => m.steam.steamid, steamid + ""));
+                var userstr = $"User {steamid} account ID {accid} hero {hero.fullName}";
+                if (user == null)
+                {
+                    var msg = $"{userstr} not found in the system.";
+                    log.Warn(msg);
+                    return msg;
+                }
+
+                if (user.vouch?.leagues == null || !user.vouch.leagues.Contains(league.Id))
+                {
+                    var msg = $"{userstr} not in league {league.Name}!";
+                    log.Warn(msg);
+                    return msg;
+                }
+
+                var team = ((8 & (1 << plyr["player_slot"].Value<byte>() - 1)) == 0) ? MatchTeam.Dire : MatchTeam.Radiant;
+                var player = new MatchPlayer(user, league.Id, league.CurrentSeason,
+                    league.SecondaryCurrentSeason.ToArray())
+                {
+                    Team = team,
+                    Hero = hero
+                };
+
+                resultPlayers.Add(new MatchResultPlayer(player));
+            }
+            matchResult.Players = resultPlayers.ToArray();
+
+            RatingCalculator.CalculateRatingDelta(matchResult);
+            matchResult.ApplyRating(league.SecondaryCurrentSeason.Concat(new[] {league.CurrentSeason}).ToArray());
+            matchResult.Save();
+
+            ChatChannel.SystemMessage(league.Id,
+                $"Match {matchResult.Id} submitted by {client.User.profile.name}. Result was {(matchResult.Result == EMatchResult.DireVictory ? "dire victory." : "radiant victory.")}");
 
             return null;
         }
